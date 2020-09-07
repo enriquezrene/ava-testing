d7c336da7b778e8efa988386563f1618ce9febc9
Rene Enriquez <=enriquezrene.ap@gmail.com>
2020-09-06 20:21:11 -0500

    Introducing macros

--- test/03-setting-tests-with-functions.js
+++ test/03-setting-tests-with-functions.js
@@ -4,20 +4,6 @@
 
 const test = require('ava');
 
-const testCases = [
-  { case: 'Testing: [0, 0, 0, 1] ==> 1', binary: [0, 0, 0, 1], expectedNumber: 1 },
-  { case: 'Testing: [0, 0, 1, 0] ==> 2', binary: [0, 0, 1, 0], expectedNumber: 2 },
-  { case: 'Testing: [0, 1, 0, 1] ==> 5', binary: [0, 1, 0, 1], expectedNumber: 5 },
-  { case: 'Testing: [1, 0, 0, 1] ==> 9', binary: [1, 0, 0, 1], expectedNumber: 9 },
-  { case: 'Testing: [0, 1, 1, 0] ==> 6', binary: [0, 1, 1, 0], expectedNumber: 6 },
-  { case: 'Testing: [1, 1, 1, 1] ==> 15', binary: [1, 1, 1, 1], expectedNumber: 15 },
-  { case: 'Testing: [1, 0, 1, 1] ==> 11', binary: [1, 0, 1, 1], expectedNumber: 11 }
-]
-
-function buildTest(testCase, t){
-  t.is(binaryArrayToNumber(testCase.binary), testCase.expectedNumber);
-}
-
 const binaryArrayToNumber = (arr) => {
   const arrayLength = arr.length;
   let number = 0;
@@ -25,10 +11,16 @@ const binaryArrayToNumber = (arr) => {
     number += arr[i] === 0 ? 0 : Math.pow(2, arrayLength - i - 1);
   }
   return number
-};
+}
 
-testCases.forEach( testCase => {
-  test(testCase.case, t => {
-    buildTest(testCase, t);
-  });
-});
+function macro(t, input, expected) {
+  t.is(binaryArrayToNumber(input), expected)
+}
+
+test('Testing: [0, 0, 0, 1] ==> 1', macro, [0, 0, 0, 1], 1);
+test('Testing: [0, 0, 1, 0] ==> 2', macro, [0, 0, 1, 0], 2);
+test('Testing: [0, 1, 0, 1] ==> 5', macro, [0, 1, 0, 1], 5);
+test('Testing: [1, 0, 0, 1] ==> 9', macro, [1, 0, 0, 1], 9);
+test('Testing: [0, 1, 1, 0] ==> 6', macro, [0, 1, 1, 0], 6);
+test('Testing: [1, 1, 1, 1] ==> 15', macro, [1, 1, 1, 1], 15);
+test('Testing: [1, 0, 1, 1] ==> 11', macro, [1, 0, 1, 1], 11);
